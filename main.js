3858f639eaf2796672ab86d9f38d668e8159650d
cosmicwanderer7 <yewaleprithvi2003@gmail.com>
2023-12-13 15:51:17 +0530

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#Button").click(function () {
+        $("#Button").animate({ width: "420.811px", padding: "15px 25px" }, "fast");
+        $("#Button").animate({ width: "397.811px", padding: "10px 20px" }, "slow");
+    });
+});
